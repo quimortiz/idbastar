4006f622fe50422a3313b9ab472e3047b23ac6b9
Khaledwahba1994 <Khaled.hassan1994@gmail.com>
2025-01-09 17:49:30 +0200

    write the opt sol for each iteration

--- include/dynoplan/optimization/payloadTransport_optimization.hpp
+++ include/dynoplan/optimization/payloadTransport_optimization.hpp
@@ -1,9 +1,11 @@
 #pragma once
-
+#include "dynobench/motions.hpp"
 #include <string>
 
 bool execute_payloadTransportOptimization(std::string &env_file,
                                     std::string &initial_guess_file,
                                     std::string &output_file,
+                                    std::string &output_file_anytime,
+                                    dynobench::Trajectory &sol,
                                     const std::string &dynobench_base,
                                     bool sum_robots_cost);
--- src/optimization/unicyclesWithRods_optimization.cpp
+++ src/optimization/unicyclesWithRods_optimization.cpp
@@ -4,14 +4,18 @@
 #include <string>
 #include <vector>
 
+  using namespace dynoplan;
+  using namespace dynobench;
+
+
 bool execute_unicyclesWithRodsOptimization(std::string &env_file,
                                     std::string &initial_guess_file,
                                     std::string &output_file,
+                                    std::string &output_file_anytime,
+                                    Trajectory &sol,
                                     const std::string &dynobench_base,
                                     bool sum_robots_cost) {
 
-  using namespace dynoplan;
-  using namespace dynobench;
 
   Result_opti result;
 
@@ -23,16 +27,16 @@ bool execute_unicyclesWithRodsOptimization(std::string &env_file,
   Problem problem(env_file);
   problem.models_base_path = dynobench_base + std::string("models/");
   Trajectory init_guess(initial_guess_file);
-  Trajectory sol;
   trajectory_optimization(problem, init_guess, options_trajopt, sol, result);
+
   // sol.to_yaml_format(output_file);
-  sol.to_yaml_format(output_file.c_str());
   
   if (result.feasible) {
     std::cout << "croco success -- returning " << std::endl;
     std::cout << "optimization done! " << std::endl;
     std::cout << "trajectory written in: " << output_file << std::endl;
-    
+    sol.to_yaml_format(output_file.c_str());  
+    sol.to_yaml_format(output_file_anytime.c_str());  
     return true;
   } else {
     std::cout << "croco failed -- returning " << std::endl;
